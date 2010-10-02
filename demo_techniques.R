7a88ce405ad2d70c6c1eba8756ccb11944a1e906
nat2zxd <nat2zxd@4e725c56-99f4-47e8-9f61-6b8c95a78723>
2010-10-02 02:34:14 +0000

    added mars and lars
    git-svn-id: http://xp-dev.com/svn/nat2zxd_r_projects/knn@134 4e725c56-99f4-47e8-9f61-6b8c95a78723

--- demo_techniques.R
+++ demo_techniques.R
@@ -26,7 +26,7 @@ lm_model <- lm (Occupancy ~ ., data=hotelv3)
 #plot(lm_model)
 
 #final data set
-hotelv4 <- subset(hotelv3,select = -c(web_nts_totsty,PercentLeisure,PercentTransientNights))
+hotelv4 <- subset(hotelv3,select = -c(web_nts_totsty,PercentLeisure,PercentTransientNights,LOC_DESC))
 
 #plot by mgmt type
 library(lattice)
@@ -47,6 +47,10 @@ hotelv4Valid <- subset(hotelv4,random>.8)
 #SAS PROC REG
 lm_model <- lm (Occupancy ~ ., data=hotelv4Train)
 #plot(lm_model)
+#fit stepwise
+#step_lm <- step(lm_model,direction="both")
+#summary(step_lm)
+
 
 #fit robust regression
 #SAS RobustReg, useful for outliers
@@ -61,14 +65,35 @@ qm1 <- rq(Occupancy ~.,tau=.5, data=hotelv4Train)
 
 #fit PLS
 # SAS PROC PLS, useful for collinearity
-hotelv4Train <- subset(hotelv4Train,select = -c(random ))
+
 library(pls)
 plsm1 <- mvr(Occupancy ~., 20,data=hotelv4Train, validation="CV")
-coef(plsm1)
+#coef(plsm1)
 #loadings(plsm1)
 #plot(RMSEP(plsm1), legendpos = "topright")
 #plot(plsm1, "loadings", comps = 1:3,legendpos = "topleft")
-summary(plsm1)
+#summary(plsm1)
+
+
+#fit penalized
+library(penalized)
+#pen <- penalized(Occupancy ~., data=hotelv4Train,model="linear", lambda1=.1,steps=50,standardize=TRUE, maxiter=100)
+#plotpath(pen,labelsize=.5)
+#coefficients(pen)
+
+#fit lars
+library(lars)
+lessocc <- subset(hotelv4Train, select = -c(Occupancy))
+lar <- lars(data.matrix(lessocc),data.matrix(hotelv4Train$Occupancy))
+plot(lar)
+
+#fit MARS
+library(earth)
+mars <- earth(Occupancy ~., data=hotelv4Train)
+summary(mars)
+#evimp(mars)
+#plot(mars)
+#plotmo(mars)
 
 #random forest, useful for prediction and variable importance
 library(randomForest)
@@ -88,7 +113,6 @@ tree1 <- rpart(Occupancy ~ ., data=hotelv4Train,method='anova',cp=.005)
 #plot(tree1, uniform=FALSE)
 #text(tree1,digits=3, cex=.7)
 
-
 #neural net, prediction
 library(nnet)
 nnet1 <- nnet(Occupancy ~ ., data=hotelv4Train,size=7,maxit=2000,decay = .001,linout=F)
@@ -100,13 +124,11 @@ svm1 <- svm(Occupancy ~., data=hotelv4Train, type='eps')
 #run GAM
 #SAS PROC GAM, functional form / non-linearities
 library(mgcv)
-gam1 <- gam(Occupancy ~ s(Compet_Occupancy) + s(AvgDailyRate,Compet_AvgDailyRate) + s(PercentGovtNights) + slf_nts_totsty + RMS_AVAIL_QTY + LOC_DESC + PercentGroupNights, data=hotelv4Train)
+gam1 <- gam(Occupancy ~ s(Compet_Occupancy) + s(AvgDailyRate,Compet_AvgDailyRate) + s(PercentGovtNights) + slf_nts_totsty + RMS_AVAIL_QTY + PercentGroupNights, data=hotelv4Train)
 summary(gam1)
 #plot(gam1)
 #vis.gam(gam1)
 #gam.check(gam1)
-
-
 
 
 #variable importance and classification
@@ -121,15 +143,17 @@ summary(gam1)
 ##plot(lmProfile, metric = "Rsquared",type="b")
 #predictors(lmProfile)
 
+#library(leaps)
+#bestreg <- regsubsets(Occupancy ~., data=hotelv4Train)
+#plot(bestreg)
 
 #R squared decomposition
 #run variance decomposition
-lm_model <- lm (Occupancy ~ Compet_Occupancy + PercentGroupNights + LOC_DESC + AvgDailyRate + PercentBusiness + slf_nts_totsty, data=hotelv4Train)
-library(relaimpo)
-a <- calc.relimp(lm_model,type = c("lmg","last", "first"), rela = TRUE)
+#lm_model <- lm (Occupancy ~ Compet_Occupancy + PercentGroupNights + AvgDailyRate + PercentBusiness + slf_nts_totsty, data=hotelv4Train)
+#library(relaimpo)
+#a <- calc.relimp(lm_model,type = c("lmg","last", "first"), rela = TRUE)
 #a <- calc.relimp(ols,type = c("lmg", "pmvd", "last", "first", "betasq", "pratt"), rela = TRUE)
 #plot(a)
-
 
 
 #predict validation data set
@@ -150,7 +174,7 @@ ptree <- predict(tree1,newdata=hotelv4Valid)
 hValid <- data.frame(hValid,ptree)
 
 ptree2 <- data.frame(predict(ptree1,newdata=hotelv4Valid))
-names(ptree2) <- ("ptree2")
+names(ptree2) <- "ptree2"
 hValid <- data.frame(hValid,ptree2)
 
 ppls <- data.frame(predict(plsm1,newdata=hotelv4Valid,ncomp=12))
@@ -166,9 +190,18 @@ hValid <- data.frame(hValid,prlm)
 psvm1 <- predict(svm1,newdata=hotelv4Valid)
 hValid <- data.frame(hValid,psvm1)
 
+pmars <- data.frame(predict(mars,newdata=hotelv4Valid))
+names(pmars) <-  "pmars"
+hValid <- data.frame(hValid,pmars)
+
+xlars <- subset(hotelv4Valid,select = -c(Occupancy))
+#plars <- data.frame(predict(lar,data.matrix(xlars), type="coef", s=17))
+plars <- data.frame(predict(lar,data.matrix(xlars), type="fit", s=17))
+hValid <- data.frame(hValid,plars[,4])
+
 
 #calculate MAD and plot fits of validation set for OLS
-par(mfrow=c(2,5))
+par(mfrow=c(2,6))
 
 with (hotelv4Valid,plot(hValid$fit,Occupancy))
 with (hotelv4Valid,lines(lowess(hValid$fit,Occupancy),col='red'))
@@ -219,7 +252,19 @@ with (hotelv4Valid,plot(hValid$psvm1,Occupancy))
 with (hotelv4Valid,lines(lowess(hValid$psvm1,Occupancy),col='red'))
 mad <- cbind(mad,"SVM" = mean(abs(hotelv4Valid$Occupancy-hValid$psvm1)))
 
-#plot MAPE
+#calculate MAD and plot fits of validation set for MARS
+with (hotelv4Valid,plot(hValid$pmars,Occupancy))
+with (hotelv4Valid,lines(lowess(hValid$pmars,Occupancy),col='red'))
+mad <- cbind(mad,"MARS" = mean(abs(hotelv4Valid$Occupancy-hValid$pmars)))
+
+
+#calculate MAD and plot fits of validation set for LARS
+with (hotelv4Valid,plot(hValid$plars,Occupancy))
+with (hotelv4Valid,lines(lowess(hValid$plars,Occupancy),col='red'))
+mad <- cbind(mad,"LARS" = mean(abs(hotelv4Valid$Occupancy-hValid$plars)))
+
+
+##plot MAPE
 order <- order(colMeans(mad),decreasing = FALSE)
 sorted <- mad[1,order]
 #barplot((as.matrix(sorted)),col="blue")
