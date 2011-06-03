fb03d0497b74950146fd8f216500b516bd22352c
nat2zxd <nat2zxd@4e725c56-99f4-47e8-9f61-6b8c95a78723>
2011-06-03 23:04:43 +0000

    git-svn-id: http://xp-dev.com/svn/nat2zxd_r_projects/knn@159 4e725c56-99f4-47e8-9f61-6b8c95a78723

--- demo_techniques_class.R
+++ demo_techniques_class.R
@@ -9,7 +9,7 @@ library(Hmisc)
 #hist(hotelv4)
 
 #####Input some data from .csv
-raw <-read.csv(file="export.txt")
+raw <-read.csv(file="sample_customer.csv")
 raw$SUCCESS_B <- NULL
 raw$IDS_BOOK_PCT <- NULL
 raw <- raw[,-1]
@@ -31,8 +31,14 @@ hotelv4Valid <- subset(hotelv4Valid, select = -c(random))
 
 #Logistic Regression
 #SAS PROC LOGISTIC
-library(Design)
-llogit <- lrm(REGISTRATION_B ~ ., data=raw)
+#library(Design)
+#llogit <- lrm(REGISTRATION_B ~ ., data=hotelv4Train)
+llogit <- glm(REGISTRATION_B ~ ., data=hotelv4Train,family=binomial())
+
+#fit stepwise minimum AIC
+steplogistic <- step(llogit)
+summary(steplogistic)
+
 
 #random forest
 library(randomForest)
@@ -58,7 +64,7 @@ nnet1 <- nnet(REGISTRATION_B ~ ., data=hotelv4Train,size=15,maxit=500,linout=F)
 
 #support vector machine
 library(e1071)
-svm1 <- svm(REGISTRATION_B ~., data=hotelv4Train, type='C')
+svm1 <- svm(REGISTRATION_B ~., data=hotelv4Train, type='C',probability=TRUE)
 
 #run GAM
 #SAS PROC GAM
@@ -67,6 +73,12 @@ gam1 <- gam(REGISTRATION_B ~ s(STAY_C) + s(WEB_BOOK_PCT) + (CRO_BOOK_PCT)+ s(LEA
 summary(gam1)
 plot(gam1)
 
+library(earth)
+mars <- earth(REGISTRATION_B ~., data=hotelv4Train,glm=list(family=binomial))
+summary(mars)
+evimp(mars)
+plot(mars)
+#plotmo(mars)
 
 ##KNN
 y <- hotelv4Train$REGISTRATION_B
@@ -79,17 +91,16 @@ lvqtest(lvq.init,hotelv4Train)
 lvq.pred2 <- olvq1(hotelv4Train,y,lvq.init)
 
 
-
 #variable importance and classification
-#SAS PROC GLMSELECT
+#SAS PROC GLMSELECT	
 library(caret)
-ctrl <- rfeControl(functions = rfFuncs, method = "cv",workers=2,verbose = FALSE,returnResamp = "final")
+ctrl <- rfeControl(functions = rfFuncs, method = "BOOT",workers=2,verbose = TRUE,returnResamp = "final",saveDetails=TRUE)
 y <- hotelv4Train$REGISTRATION_B
 x <- subset(hotelv4Train,select = -c(REGISTRATION_B))
 subsets <- c(1:10)
 lmProfile <- rfe(x, y,sizes = subsets,rfeControl = ctrl)
 lmProfile
-plot(lmProfile, metric = "Rsquared",type="b")
+plot(lmProfile, metric = "Accuracy",type="b")
 predictors(lmProfile)
 
 
@@ -98,7 +109,7 @@ predictors(lmProfile)
 rf.pred2 <- data.frame(predict(rf1,newdata=hotelv4Valid,type="prob")[,2])
 names(rf.pred2) <- 'rf.pred2'
 
-logit.pred2 <- data.frame(predict(llogit,newdata=hotelv4Valid, type="fitted"))
+logit.pred2 <- data.frame(predict(llogit,newdata=hotelv4Valid, type="response"))
 names(logit.pred2) <- 'logit.pred2'
 
 nnet.pred2 <- data.frame(predict(nnet1,newdata=hotelv4Valid,type="raw"))
@@ -110,39 +121,54 @@ names(gam.pred2) <- 'gam.pred2'
 ptree.pred2 <- data.frame(predict(ptree1,newdata=hotelv4Valid))
 names(ptree.pred2) <- 'ptree.pred2'
 
-svm.pred2 <- data.frame(predict(svm1,newdata=hotelv4Valid))
+svm.pred2 <- predict(svm1,newdata=hotelv4Valid,probability=TRUE)
+svm.pred2 <- data.frame(attr(svm.pred2,'probabilities')[,1])
 names(svm.pred2) <- 'svm.pred2'
 
+mars.pred2 <- data.frame(predict(mars,newdata=hotelv4Valid,type="response"))
+names(mars.pred2) <- 'mars.pred2'
+
 lvq.pred2 <- lvqtest(lvq.pred2,hotelv4Valid)
+
+random.pred2 <- runif(nrow(hotelv4Valid))
+
 
 library(ROCR)
 plot(performance(prediction(rf.pred2,hotelv4Valid$REGISTRATION_B),"tpr","fpr"),col="red")
 plot(performance(prediction(logit.pred2,hotelv4Valid$REGISTRATION_B),"tpr","fpr"),col="blue",add=TRUE)
 plot(performance(prediction(nnet.pred2,hotelv4Valid$REGISTRATION_B),"tpr","fpr"),col="purple",add=TRUE)
 plot(performance(prediction(gam.pred2,hotelv4Valid$REGISTRATION_B),"tpr","fpr"),col="green",add=TRUE)
-
+plot(performance(prediction(svm.pred2,hotelv4Valid$REGISTRATION_B),"tpr","fpr"),col="yellow",add=TRUE)
+plot(performance(prediction(mars.pred2,hotelv4Valid$REGISTRATION_B),"tpr","fpr"),col="black",add=TRUE)
+plot(performance(prediction(random.pred2,hotelv4Valid$REGISTRATION_B),"tpr","fpr"),col="orange",add=TRUE)
 
 auc1 <- performance(prediction(rf.pred2,hotelv4Valid$REGISTRATION_B),"auc")@y.values[[1]]
 auc2 <- performance(prediction(logit.pred2,hotelv4Valid$REGISTRATION_B),"auc")@y.values[[1]]
 auc3 <- performance(prediction(gam.pred2,hotelv4Valid$REGISTRATION_B),"auc")@y.values[[1]]
 auc4 <- performance(prediction(nnet.pred2,hotelv4Valid$REGISTRATION_B),"auc")@y.values[[1]]
-legend("bottomright",legend=c(paste("Random Forest         (AUC=",formatC(auc1,digits=4,format="f"),")",sep=""),
-				paste("Logistic Regression  (AUC=",formatC(auc2,digits=4,format="f"),")",sep=""),
+auc5 <- performance(prediction(svm.pred2,hotelv4Valid$REGISTRATION_B),"auc")@y.values[[1]]
+auc6 <- performance(prediction(mars.pred2,hotelv4Valid$REGISTRATION_B),"auc")@y.values[[1]]
+auc7 <- performance(prediction(random.pred2,hotelv4Valid$REGISTRATION_B),"auc")@y.values[[1]]
+
+legend("bottomright",legend=c(paste("Random Forest  (AUC=",formatC(auc1,digits=4,format="f"),")",sep=""),
+				paste("Logistic Regression  		(AUC=",formatC(auc2,digits=4,format="f"),")",sep=""),
 				paste("GAM                          (AUC=",formatC(auc3,digits=4,format="f"),")",sep=""),
-				paste("Neural Net                 (AUC=",formatC(auc4,digits=4,format="f"),")",sep="")),
-				col=c("red","blue","green","purple"), lty=1)
+				paste("Neural Net                 	(AUC=",formatC(auc4,digits=4,format="f"),")",sep=""),
+				paste("SVM		                 	(AUC=",formatC(auc5,digits=4,format="f"),")",sep=""),
+				paste("MARS		                 	(AUC=",formatC(auc6,digits=4,format="f"),")",sep=""),
+				paste("Random		                (AUC=",formatC(auc7,digits=4,format="f"),")",sep="")),
+				col=c("red","blue","green","purple","yellow","black","orange"), lty=.5)
 
 
 
 library(gmodels)
 
-svm.predict=as.numeric(as.character(svm.pred2$svm.pred2))
+svm.predict <- as.numeric(as.character(svm.pred2$svm.pred2))
+ptree.predict <- as.numeric(as.character(ptree.pred2$ptree.pred2))
+
 CrossTable(svm.predict,hotelv4Valid$REGISTRATION_B,format="SAS",prop.r="FALSE",prop.c="FALSE",prop.chisq="FALSE")
-
 CrossTable(lvq.pred2,hotelv4Valid$REGISTRATION_B,format="SAS",prop.r="FALSE",prop.c="FALSE",prop.chisq="FALSE")
-
-ptree.predict=as.numeric(as.character(ptree.pred2$ptree.pred2))
-CrossTable(ptree.predict,hotelv4Valid$REGISTRATION_B,format="SAS",prop.r="FALSE",prop.c="FALSE",prop.chisq="FALSE")
+CrossTable(ptree.predict,hotelv4Valid$REGISTRATION_B,format="SPSS",prop.r="FALSE",prop.c="FALSE",prop.chisq="FALSE")
 
 
 
