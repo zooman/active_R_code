464ed042928239a1a7b780dbe25a7cdbc9f94f18
nat2zxd <nat2zxd@4e725c56-99f4-47e8-9f61-6b8c95a78723>
2011-06-03 23:00:57 +0000

    git-svn-id: http://xp-dev.com/svn/nat2zxd_r_projects/knn@156 4e725c56-99f4-47e8-9f61-6b8c95a78723

--- wag.R
+++ wag.R
@@ -35,39 +35,41 @@ raw.train <- subset(raw,random <=.8, select = -c(random))
 raw.valid <- subset(raw,random >.8, select = -c(random))
 
 library(randomForest)
-rf1 <- randomForest(PredictedClass ~ ., data=raw.train, ntree=50, importance=TRUE)
+rf1 <- randomForest(PredictedClass ~ ., data=raw.train, ntree=100, importance=TRUE)
 plot(rf1, legend=TRUE)
-legend("bottomleft","x,y")
+#legend("bottomleft","x,y")
 varImpPlot(rf1)
 impRF <- importance(rf1,type=1)
 
 #run tree CART algorithm
 library(rpart)
-tree1 <- rpart(PredictedClass ~ ., data=raw.train,method='class',cp=.005,xval=30)
+tree1 <- rpart(PredictedClass ~ ., data=raw.train,method='class',cp=.005,xval=20)
 printcp(tree1)
 plot(tree1, uniform=FALSE)
 text(tree1,digits=3, cex=.5)
 
-printcp(tree1)
 plotcp(tree1)
-summary(tree1)
+summary(tree1) 
+print(tree1)# has the RULES..
 
-#pruneTree <- prune(tree1, cp=tree1$cptable[which.min(tree1$cptable[,"xerror"]),"CP"])
+pruneTree <- prune(tree1, cp=tree1$cptable[which.min(tree1$cptable[,"xerror"]),"CP"])
 #plot(pruneTree, uniform=FALSE, main = "Pruned")
 #text(pruneTree,digits=3, cex=.5)
-
 #par(mfrow=c(2,2))
 #rsq.rpart(tree1)
 #rsq.rpart(pruneTree)
+
+
+#support vector machine
+#library(e1071)
+#svm1 <- svm(PredictedClass ~ ., data=raw.train, type='C')
+
 
 #run regression tree
 #library(party)
 #ptree1 <- ctree(PredictedClass ~ ., controls=ctree_control(mincriterion=.90), data=raw.train)
 #plot(ptree1, main="WAG Chronic Tree", type="simple")
 
-#support vector machine
-#library(e1071)
-#svm1 <- svm(PredictedClass ~ ., data=raw.train, type='C')
 
 #library(nnet)
 #mlogit1 <- multinom(PredictedClass ~ ., data=raw.train,MaxNWts = 5000, maxit=500)
@@ -84,15 +86,15 @@ summary(tree1)
 #predictors(lmProfile)
 
 
-library(gmodels)
 forest.predict <- as.numeric(as.character(predict(rf1,newdata=raw.valid),type="class"))
-#svm.predict <- as.numeric(as.character(predict(svm1,newdata=raw.valid, type='class')))
 tree.predict <- as.numeric(as.character(predict(tree1,newdata=raw.valid,type="class")))
-#prunetree.predict <- as.numeric(as.character(predict(pruneTree,newdata=raw.valid,type="class")))
+prunetree.predict <- as.numeric(as.character(predict(pruneTree,newdata=raw.valid,type="class")))
+#svm.predict <- as.numeric(as.character(predict(svm1,newdata=raw.valid, type='class')))
 
+library(gmodels)
 #CrossTable(svm.predict,raw.valid$PredictedClass,format="SPSS",prop.r="TRUE",prop.c="TRUE",prop.chisq="FALSE")
-CrossTable(tree.predict,raw.valid$PredictedClass,format="SPSS",prop.r="TRUE",prop.c="TRUE",prop.chisq="FALSE")
-CrossTable(forest.predict,raw.valid$PredictedClass,format="SPSS",prop.r="TRUE",prop.c="TRUE",prop.chisq="FALSE",mcnemar="TRUE")
-#CrossTable(prunetree.predict,raw.valid$PredictedClass,format="SPSS",prop.r="TRUE",prop.c="TRUE",prop.chisq="FALSE")
+CrossTable(tree.predict,raw.valid$PredictedClass,format="SPSS",prop.r="FALSE",prop.c="TRUE",prop.chisq="FALSE")
+CrossTable(forest.predict,raw.valid$PredictedClass,format="SPSS",prop.r="FALSE",prop.c="TRUE",prop.chisq="FALSE")
+CrossTable(prunetree.predict,raw.valid$PredictedClass,format="SPSS",prop.r="FALSE",prop.c="TRUE",prop.chisq="FALSE")
 
 #mlogit1 <- multinom(PredictedClass ~ TOTALCOND, data=raw,MaxNWts = 5000, maxit=500)
